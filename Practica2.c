cd5d09fbf55295e26c601e4802da4d26a2c59e83
root <root@debian>
2017-01-24 08:13:13 -0600

    prueba

---
+++ Practica2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<limits.h>
+//Practica 2
+//Martinez Casares Jenifer
+char *cadena = "hola, Mundo\n";
+int main()
+{
+	printf("%d, El valor máximo  de un caracter con signo:\n", SHRT_MAX );
+	printf("%ld, El valor de un entero corto\n", SHRT_MIN);
+	printf("%d, El valor minimo de un entero largo \n", LONG_MIN );
+	printf("%d, El valor máximo de u  carácter con signo\n", CHAR_MAX);
+	printf("%d, El valor máximo de un entero sin signo largo\n", ULONG_MAX);
+
+}
+
+
---
+++ practica7.c
@@ -0,0 +1,17 @@
+/*Practica 7
+Martínez Casares Jenifer
+"Cifrado con xor"
+*/
+#include <stdio.h>
+#include <stdlib.n>
+#include <string.h>
+int main(){
+char *palabra="cifrado"
+char *palabraCifrada=(char *) calloc(size of(char),60);
+int llave=50;
+	int i;
+	for(i=0;<sizeof(palabra)/sizeof(char);i++){
+	palabraCifrada[i]= palabra[i]
+	}
+}
+
